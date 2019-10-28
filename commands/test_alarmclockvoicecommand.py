3ec6d0e93b1d83aab34c90d7ee04587f60d9257a
derHeinz <heinzserfas@gmx.de>
2019-10-28 10:02:07 +0100

    made executable

--- commands/test_alarmclockvoicecommand.py
+++ commands/test_alarmclockvoicecommand.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
-from .alarmclockvoicecommand import AlarmClockVoiceCommand
+import os
+from .voicecommand import VoiceCommand
 
-class TestAlarmClockVoiceCommand(unittest.TestCase):
+class AlarmClockVoiceCommand(VoiceCommand):
 
-    def _testee(sef):
-        return AlarmClockVoiceCommand({"ip": "1.1.1.1"})
+    START_TOKEN = ["Stelle Wecker auf", "Wecker auf", "Stelle Wecker um"]
+    OFF_TOKEN = ["Wecker aus"]
+    
+    def __init__(self, config=None):
+        self.START_TOKEN.sort(key=len, reverse=True)
+        if (config == None):
+            self._load_config_file()
+        else:
+            self._load_config(config)
         
-    def test_can_process(self):
-        positive_list = ["Wecker auf 8", "Wecker auf acht", "Stelle Wecker auf 6 Uhr 30", 
-        "Stelle Wecker auf 6 Uhr dreißg", "Stelle Wecker auf sechs Uhr dreißig"]
-        
-        for text in positive_list:
-            self.assertTrue(self._testee().can_process(text))
+    def _load_config_file(self):
+        config_filename = os.path.dirname(__file__) + "/alarmclock.json" 
+        with open(config_filename) as data_file:    
+            data = json.load(data_file)
+            self._load_config(data)
             
-        negative_list = ["weck mich um 6", "Wecker um sieben Uhr", "Wecken um zehn"]
-        for text in negative_list:
-            self.assertFalse(self._testee().can_process(text))
-            
-    def test_is_deactivate(self):
-        self.assertTrue(self._testee()._is_deactivate("Wecker aus"))
-        self.assertTrue(self._testee()._is_deactivate("wecker aus"))
-            
-    def test_extract_time(self):
-        self.assertEqual("8:00", self._testee()._extract_time("Wecker auf 8 Uhr"))
-        self.assertEqual("8:00", self._testee()._extract_time("Wecker auf 8"))
-        self.assertEqual("10:20", self._testee()._extract_time("Stelle Wecker auf 10 Uhr 20"))
+    def _load_config(self, data):
+        self.ALARMCLOCKIP = data['ip']
+    
+    def can_process(self, vc):
+        for keyword in self.START_TOKEN:
+            if vc.lower().startswith(keyword.lower()):
+                return True
+        for keyword in self.OFF_TOKEN:
+            if (vc.lower() == keyword.lower()):
+                return True
+        return False
         
-        self.assertEqual("11:30", self._testee()._extract_time("Stelle Wecker auf 11 30"))
-        self.assertEqual("11:30", self._testee()._extract_time("Stelle Wecker auf 11 30 Uhr"))
-        
-        self.assertEqual("11:30", self._testee()._extract_time("Wecker auf elf dreißig"))
-        self.assertEqual("11:30", self._testee()._extract_time("Stelle Wecker auf elf dreißig"))
-        self.assertEqual("11:30", self._testee()._extract_time("Stelle Wecker auf elf dreißig Uhr"))
+    NUMBERS = {"eins": "1", "zwei": "2", "drei": "3", "vier": "4", "fünf": "5", 
+    "sechs": "6", "sieben": "7", "acht": "8", "neun": "9", "zehn": "10", "elf": "11",
+    "zwölf": "12", "dreizehn": "13", "vierzehn": "14", "fünfzehn": "15", "sechszehn": "16",
+    "siebzehn": "17", "achtzehn": "18", "neunzehn": "11", "zwanzig": "20", "einundzwanzig": "21",
+    "zweundzwanzig": "22", "dreiundzwanzig": "23", "dreißig": "30", "vierzig": "40", "fünfzig": "50"}
+
+    def _number_replace(self, time):
+        t = time.strip().lower()
+        if (t in self.NUMBERS):
+            return self.NUMBERS[t]
+        return time
+    
+    def _legal_time_
